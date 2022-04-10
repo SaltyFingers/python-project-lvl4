f45a2e6069ed4a4a86a55b6b75f590892244106c
Evchik <elcric66@gmail.com>
2022-04-11 02:19:55 +0300

    step 2 try do something with bootstrap

--- task_manager/task_manager/views.py
+++ task_manager/task_manager/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from multiprocessing import context
+from django.views.generic import TemplateView
 
 
-def index(request):
-    return render(request, 'index.html', context={
-        'who': 'World',
-    })
+class IndexView(TemplateView):
+    template_name = 'index.html'
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(IndexView, self).get_context_data(*args, **kwargs)
+        context['who'] = 'World'
+        return context
+# def index(request):
+#     return render(request, 'index.html', context={
+#         'who': 'World',
+#     })
+
+
+
