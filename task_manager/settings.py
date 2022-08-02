5dbda7aa859444f61f64c674569da621168571c9
Evchik <elcric66@gmail.com>
2022-08-02 20:27:14 +0300

    edit settings 3

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -29,9 +29,8 @@ FIXTURE_DIRS = (os.path.join(BASE_DIR, "fixtures"),)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-load_dotenv()
-
-SECRET_KEY = os.getenv("SECRET_KEY")
+load_dotenv(find_dotenv())
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
