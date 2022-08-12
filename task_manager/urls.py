4a63162c47753df6b3ede7a999b24c49a7dd0296
Evchik <elcric66@gmail.com>
2022-08-12 17:07:13 +0300

    edit settings and urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -21,8 +21,8 @@ from task_manager.views import IndexView, LoginView, LogoutView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", IndexView.as_view(), name="index"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path("login/", LoginView.as_view(next_page="index"), name="login"),
+    path("logout/", LogoutView.as_view(next_page="index"), name="logout"),
     path("users/", include("task_manager.users.urls"), name="users"),
     path("statuses/", include("task_manager.statuses.urls"), name="statuses"),
     path("tasks/", include("task_manager.tasks.urls"), name="tasks"),
